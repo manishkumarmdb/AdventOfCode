124ac15a71315ed4c131a23e55b850e786df73a8
manishkumarmdb <manishkumarmdb@gmail.com>
2018-07-09 17:16:11 +0530

    write function for read input file

--- Clojure/adventofcode2017/src/adventofcode2017/core.clj
+++ Clojure/adventofcode2017/src/adventofcode2017/core.clj
@@ -1,5 +1,14 @@
 (ns adventofcode2017.core
-  (:gen-class))
+	(:gen-class))
+
+
+(defn read-input-from-file
+	""
+	[path]
+	(with-open [rdr (clojure.java.io/reader path
+                             :append true
+                             :encoding "UTF-8")]
+		(doall (line-seq rdr))))
 
 (defn -main
   "I don't do a whole lot ... yet."
