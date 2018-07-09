ee6a308a97c6352d4bc15bfa56dbd7ebd8bdb0e9
manishkumarmdb <manishkumarmdb@gmail.com>
2018-07-09 17:20:24 +0530

    add core namespace and update high-entropy-passphrases-part1 & high-entropy-passphrases-part2 function

--- Clojure/adventofcode2017/src/adventofcode2017/day004.clj
+++ Clojure/adventofcode2017/src/adventofcode2017/day004.clj
@@ -1,22 +1,10 @@
-(ns adventofcode2017.day004)
+(ns adventofcode2017.day004
+	(:require [adventofcode2017.core :as core]))
 
 
 ;;--- Day 4: High-Entropy Passphrases ---
 ;;===========================
 ;; part 1
-
-(def path "/home/manish/manish/manishkumarmdb@github/AdventOfCode/Clojure/adventofcode2017/input/day004.txt")
-
-;;(def path (atom ""))
-
-
-(defn read-input-and-put-in-seq-line-by-line
-	""
-	[path]
-	(with-open [rdr (clojure.java.io/reader path
-                             :append true
-                             :encoding "UTF-8")]
-		(doall (line-seq rdr))))
 
 (defn split-passphrase-by-space
 	""
@@ -31,11 +19,9 @@
 
 (defn high-entropy-passphrases-part1
 	""
-	[input-file-path]
-	(->> input-file-path 
-		(read-input-and-put-in-seq-line-by-line)
-		(map (fn [x] (->> 
-						x 
+	[coll]
+	(->> coll
+		(map (fn [x] (->> x 
 						(split-passphrase-by-space)
 						(passphrase1?))))
 		(filter true?)
@@ -53,11 +39,9 @@
 
 (defn high-entropy-passphrases-part2
 	""
-	[input-file-path]
-	(->> input-file-path
-		(read-input-and-put-in-seq-line-by-line)
-		(map (fn [x] (->>
-						x
+	[coll]
+	(->> coll
+		(map (fn [x] (->> x
 						(split-passphrase-by-space)
 						(passphrase2?))))
 		(filter true?)
